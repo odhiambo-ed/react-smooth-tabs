7c6eab7a372b2f97fd9137e17a7d7dee4fd6b888
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 22:57:18 +0300

    add proptype

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,13 +1,22 @@
+import PropTypes from "prop-types";
 
-
-function Tab({isActive, onClick, label}) {
+function Tab({ isActive, onClick, label }) {
   return (
-      <li className="nav-item">
-          <button className={`nav-link ${isActive ? 'active' : ''}`} onClick={onClick}>
-              { label }
-          </button>
+    <li className="nav-item">
+      <button
+        className={`nav-link ${isActive ? "active" : ""}`}
+        onClick={onClick}
+      >
+        {label}
+      </button>
     </li>
-  )
+  );
 }
 
-export default Tab
+Tab.propTypes = {
+  isActive: PropTypes.bool.isRequired, 
+  onClick: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired, 
+};
+
+export default Tab;
