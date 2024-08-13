c42bc9675fcd811f63e7f4ff68832dc2ca5d3703
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 20:29:10 +0300

    add tabs components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Tabs from './components/Tabs'
 
 function App() {
 
   return (
     <>
-      <h1>Jesus in You I trust</h1>
+      <Tabs />
     </>
   )
 }
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+
+
+function Tabs() {
+  return (
+      <div className="container">
+          <ul className="nav">
+              
+          </ul>
+    </div>
+  )
+}
+
+export default Tabs
