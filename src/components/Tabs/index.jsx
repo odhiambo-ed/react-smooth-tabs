61fb0f60b13e3e86d2a43d64cc7ec32003919c23
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 22:43:27 +0300

    add more tabs

--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -3,22 +3,47 @@ import Tab from "./Tab"
 
 
 function index() {
-    const [activeTab, setActiveTab] = useState(introduction)
+    const [activeTab, setActiveTab] = useState("introduction")
 
     const handleClick = (item) => {
         setActiveTab(item)
     }
   return (
-      <div className="container">
-          <ul className="nav nav-tabs">
-              <Tab
-                  label={'Introduction'}
-                  onClick={() => handleClick(introduction)}
-                  isActive={activeTab === 'Introduction'}
-              />
-          </ul>
+    <div className="container">
+      <ul className="nav nav-tabs">
+        <Tab
+          label={"Introduction"}
+          onClick={() => handleClick("introduction")}
+          isActive={activeTab === "Introduction"}
+        />
+        <Tab
+          label="Components"
+          isActive={activeTab === "components"}
+          onClick={() => handleTabClick("components")}
+        />
+        <Tab
+          label="State & Props"
+          isActive={activeTab === "state-props"}
+          onClick={() => handleTabClick("state-props")}
+        />
+        <Tab
+          label="Lifecycle Methods"
+          isActive={activeTab === "lifecycle-methods"}
+          onClick={() => handleTabClick("lifecycle-methods")}
+        />
+        <Tab
+          label="Hooks"
+          isActive={activeTab === "hooks"}
+          onClick={() => handleTabClick("hooks")}
+        />
+        <Tab
+          label="Advanced Topics"
+          isActive={activeTab === "advanced-topics"}
+          onClick={() => handleTabClick("advanced-topics")}
+        />
+      </ul>
     </div>
-  )
+  );
 }
 
 
