57ee8f2d6f9ce8886e6cc325884bcb0fcc546316
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-15 08:42:35 +0300

    add backgroud

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Tabs from './components/Tabs'
 function App() {
 
   return (
-    <div className="container mt-4 justify-content-center align-items-center">
+    <div className="container mt-4 justify-content-center align-items-center bg-light vw-100 vh-100">
       <Tabs />
     </div>
   );
